b0266955424a90c041e40d8329ad3eba01bfea2f
YulianGuinand <yulianguinand365@gmail.com>
2022-12-20 16:21:33 +0100

    fourth

--- contact.php
+++ contact.php
@@ -27,24 +27,3 @@ if (isset($_POST['submit'])) {
 }
 
 ?>
-
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Formulaire de contact</title>
-</head>
-<body>
-  <?php if (isset($success) && $success): ?>
-    <p>Votre message a été envoyé avec succès !</p>
-  <?php elseif (isset($error) && $error): ?>
-    <p>Une erreur s'est produite lors de l'envoi de votre message.</p>
-  <?php else: ?>
-    <form action="" method="post">
-        <input type="text" id="name" name="name" class="contact__text" placeholder="Nom :"><br>
-        <input type="text" id="email" name="email" class="contact__text" placeholder="Email :"><br>
-        <textarea id="message" name="message" class="contact__text" placeholder="Message"></textarea><br>
-        <input class="contact__btn" name="submit" type="submit" value="Envoyer">
-      </form>
-  <?php endif; ?>
-</body>
-</html>
