b78b0fafb91836760cd43272a429ec69b5d5b85c
YulianGuinand <yulianguinand365@gmail.com>
2022-12-20 16:16:28 +0100

    third

--- contact.php
+++ contact.php
@@ -1,15 +1,50 @@
 <?php
-if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['message'])) {
+
+// Vérifie si le formulaire a été soumis
+if (isset($_POST['submit'])) {
+  // Récupère les données du formulaire
   $name = $_POST['name'];
   $email = $_POST['email'];
   $message = $_POST['message'];
-  $to = "yulianguinand365@gmail.com";
-  $subject = "Email depuis CCSVA Tennis de table;
-  $headers = "From: $email" . "\r\n" .
-             "CC: another@email.com";
-  $mailBody = "Name: $name\nEmail: $email\n\n$message";
-  
-  mail($to, $subject, $mailBody, $headers);
-  echo "Votre message a été envoyé !";
+
+  // Valide les données du formulaire
+  $name = htmlspecialchars($name);
+  $email = htmlspecialchars($email);
+  $message = htmlspecialchars($message);
+
+  // Prépare le corps du message
+  $to = 'yulianguinand365@gmail.com';
+  $subject = 'Nouveau message de la part de '.$name;
+  $message = 'De : '.$name.' ('.$email.')'."\r\n\r\n".$message;
+  $headers = 'From: '.$email."\r\n".'Reply-To: '.$email."\r\n".'X-Mailer: PHP/'.phpversion();
+
+  // Envoie le message
+  if (mail($to, $subject, $message, $headers)) {
+    $success = true;
+  } else {
+    $error = true;
+  }
 }
+
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Formulaire de contact</title>
+</head>
+<body>
+  <?php if (isset($success) && $success): ?>
+    <p>Votre message a été envoyé avec succès !</p>
+  <?php elseif (isset($error) && $error): ?>
+    <p>Une erreur s'est produite lors de l'envoi de votre message.</p>
+  <?php else: ?>
+    <form action="" method="post">
+        <input type="text" id="name" name="name" class="contact__text" placeholder="Nom :"><br>
+        <input type="text" id="email" name="email" class="contact__text" placeholder="Email :"><br>
+        <textarea id="message" name="message" class="contact__text" placeholder="Message"></textarea><br>
+        <input class="contact__btn" name="submit" type="submit" value="Envoyer">
+      </form>
+  <?php endif; ?>
+</body>
+</html>
